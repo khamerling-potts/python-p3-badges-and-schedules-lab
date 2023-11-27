935ebdf6f3b4c5b1b0452d20aa84254c65d386cc
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-11-27 11:37:16 -0500

    done

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,21 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
+
 
 def assign_rooms(names):
-    return None
+    rooms = []
+    for i in range(0, 8):
+        rooms.append(f"Hello, {names[i]}! You'll be assigned to room {i+1}!")
+    return rooms
+
 
 def printer(names):
+    for badge in batch_badge_creator(names):
+        print(badge)
+    for room in assign_rooms(names):
+        print(room)
     return None
